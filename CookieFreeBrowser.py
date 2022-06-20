97210e0537d881057d3cbd46ee369295aafd819b
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-06-20 15:07:05 -0400

    Update CookieFreeBrowser.py

--- CookieFreeBrowser.py
+++ CookieFreeBrowser.py
@@ -2,6 +2,6 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-serv = Service(r"/home/rsudo/Downloads/geckodriver")
+serv = Service(r"PathToGeckoDriver")
 driver = webdriver.Firefox(service=serv)
 

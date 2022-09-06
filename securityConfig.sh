727859dcc3156e406c6f58750c90f9126b78da3e
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-09-06 11:28:42 -0400

    Update securityConfig.sh

--- securityConfig.sh
+++ securityConfig.sh
@@ -2,16 +2,20 @@
 # a simple security baseline configuration for linux personal devices using apt
 echo "starting"
 
-sudo apt install -y clamav clamav-daemon clamtk firewalld rkhunter
+sudo apt install -y clamav clamav-daemon clamtk firewalld rkhunter ufw
 
 sudo freshclam
-
 sudo systemctl start clamav-freshclam.service
 sudo systemctl enable clamav-freshclam.service
 sudo systemctl start clamav-daemon.service
 sudo systemctl enable clamav-daemon.service
-sudo systemctl start firewalld
-sudo systemctl enable firewalld
+sudo ufw default deny incoming  
+sudo ufw default allow outgoing
+sudo ufw limit 22/tcp  
+sudo ufw allow 80/tcp  
+sudo ufw allow 443/tcp  
+sudo ufw enable
+
 sudo rkhunter -sk -c
 clamscan -r --bell --remove /
 

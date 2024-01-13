2e9c41fa78c79338ccdbe2f0552e42358f9fa4c7
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2024-01-12 19:49:01 -0500

    Delete CookieFreeBrowser directory

--- CookieFreeBrowser/CookieFreeBrowser.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-import os
-path = "{}/geckodriver".format(os.getcwd())
-serv = Service(path)
-driver = webdriver.Firefox(service=serv)
